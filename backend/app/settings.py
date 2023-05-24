75cab07aad04b0ddbb31ee0c36767a774916cd32
Daniel Kamar <daniel@koitoror.ml>
2023-05-24 19:07:50 +0000

    deploy: railway static serve

--- backend/app/settings.py
+++ backend/app/settings.py
@@ -119,9 +119,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-# STATIC_URL = "static/"
-# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles'), os.path.join(BASE_DIR, 'static/admin', os.path.join(BASE_DIR, '/'))]
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = "static/"
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles'), os.path.join(BASE_DIR, 'static/admin', os.path.join(BASE_DIR, '/'))]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STORAGES = {
     "staticfiles": {
         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
