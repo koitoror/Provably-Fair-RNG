361188c2c8b3c1f2e66351a7cc7ce36fd6fa4d60
Daniel Kamar <kamarster@gmail.com>
2023-05-30 13:24:39 +0000

    lint: remove debug code

--- backend/app/settings.py
+++ backend/app/settings.py
@@ -40,7 +40,6 @@ def removeSquareBrackets(x):
     if x.endswith(']'): x = x[:-1]
     return str(x)
 CSRF_TRUSTED_ORIGINS += list(map(removeSquareBrackets, os.environ.get('CSRF_TRUSTED_ORIGINS').split(',') ))
-print('CSRF_TRUSTED_ORIGINS → ', CSRF_TRUSTED_ORIGINS)
 
 # Application definition
 
