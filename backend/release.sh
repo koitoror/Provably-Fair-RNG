9c0ed035d617867280f816cfacc42dfa45f43216
Daniel Kamar <kamarster@gmail.com>
2023-08-04 13:24:56 +0300

    Update release.sh

--- backend/release.sh
+++ backend/release.sh
@@ -4,6 +4,16 @@ set -e
 
 whoami
 
+# alias python3.9 available in vercel
+alias python='python3.9'
+
+# Update pip
+python -m pip install --upgrade pip
+# Install requirements
+pip install -r requirements.txt
+
+# Collect Static Files on Deploy
+python manage.py collectstatic
 
 echo "Running Database migrations and migrating the new changes"
 python manage.py makemigrations 
@@ -14,6 +24,5 @@ python manage.py migrate --noinput
 
 echo "Running Server"
 gunicorn --chdir app wsgi:application --log-file - --log-level debug
-
 
 echo "Done.."
